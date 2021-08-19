e091d5b14e582fb4f4474a8667ab9f68d6931eb1
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-20 02:51:14 +0430

    refactor: do functions into the child component instead of doing into the father component
    Now the handleCompletedChange and handleDelete functions are declared into the TodosList child component instead of TodoApp father component.

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -5,31 +5,6 @@ import TodosList from "./TodosList";
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
 
-  const handleCompletedChange = (todoId) => {
-    let newTodos = [...todos];
-    const index = todos.findIndex((element) => element.id === todoId);
-    newTodos[index] = {
-      ...newTodos[index],
-      isCompleted: !newTodos[index].isCompleted,
-    };
-    setTodos(newTodos);
-
-    /* another way to changing the object in the array 
-    const newTodos = todos.map((todo) => {
-      if (todo.id === todoId) {
-        return { ...todo, isCompleted: !todo.isCompleted };
-      } else {
-        return todo;
-      }
-    });
-    setTodos(newTodos); */
-  };
-
-  const handleDelete = (todoId) => {
-    const newTodos = todos.filter((todo) => todo.id !== todoId);
-    setTodos(newTodos);
-  };
-
   return (
     <div className="page-content page-container" id="page-content">
       <div className="row container d-flex justify-content-center">
@@ -38,11 +13,7 @@ const TodoApp = () => {
             <div className="card-body">
               <h4 className="card-title">Simple Todo App</h4>
               <InputForm setTodos={setTodos} />
-              <TodosList
-                todos={todos}
-                onCompletedChange={handleCompletedChange}
-                onDelete={handleDelete}
-              />
+              <TodosList todos={todos} setTodos={setTodos} />
             </div>
           </div>
         </div>
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,6 +1,30 @@
 import React from "react";
-const TodosList = ({ todos, onCompletedChange, onDelete }) => {
-  console.log("Todos", todos);
+const TodosList = ({ todos, setTodos }) => {
+  const handleCompletedChange = (todoId) => {
+    let newTodos = [...todos];
+    const index = todos.findIndex((element) => element.id === todoId);
+    newTodos[index] = {
+      ...newTodos[index],
+      isCompleted: !newTodos[index].isCompleted,
+    };
+    setTodos(newTodos);
+
+    /* another way to changing the object in the array 
+      const newTodos = todos.map((todo) => {
+        if (todo.id === todoId) {
+          return { ...todo, isCompleted: !todo.isCompleted };
+        } else {
+          return todo;
+        }
+      });
+      setTodos(newTodos); */
+  };
+
+  const handleDelete = (todoId) => {
+    const newTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="list-wrapper">
       <ul className="d-flex flex-column todo-list">
@@ -13,7 +37,7 @@ const TodosList = ({ todos, onCompletedChange, onDelete }) => {
                   className="checkbox"
                   type="checkbox"
                   checked={todo.isCompleted ? true : false}
-                  onChange={() => onCompletedChange(todo.id)}
+                  onChange={() => handleCompletedChange(todo.id)}
                 />
                 {todo.title}
                 <i className="input-helper"> </i>
@@ -21,7 +45,7 @@ const TodosList = ({ todos, onCompletedChange, onDelete }) => {
             </div>
             <i
               className="remove mdi mdi-close-circle-outline"
-              onClick={() => onDelete(todo.id)}
+              onClick={() => handleDelete(todo.id)}
             ></i>
           </li>
         ))}
